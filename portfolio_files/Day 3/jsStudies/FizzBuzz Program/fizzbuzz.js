610a9abe320631f5d34e8093611ddb70a197b2a6
buck-nicholas <buck.nicholas.c@gmail.com>
2018-06-07 10:20:46 -0500

    Added Special Case Counter To FizzBuzz Program.

--- portfolio_files/Day 3/jsStudies/FizzBuzz Program/fizzbuzz.js
+++ portfolio_files/Day 3/jsStudies/FizzBuzz Program/fizzbuzz.js
@@ -1,16 +1,24 @@
 "use strict";
+let buzzCounter = 0;
+let fizzCounter = 0;
+let fizzBuzzCounter = 0;
 
 for(let i = 1; i <= 100; i++) {
   if ((i % 3 === 0) && (i % 5 === 0)) {
     console.log("fizzbuzz");
+    fizzBuzzCounter++;
   }
   else if (i % 3 === 0) {
     console.log("fizz");
+    fizzCounter++;
   }
   else if (i % 5 === 0) {
     console.log("buzz");
+    buzzCounter++;
   }
   else {
     console.log(i);
   }
 }
+
+console.log("Amount of Fizz: " + fizzCounter + ". " + "Amount of Buzz: " + buzzCounter + ". " + "Amount of Fizzbuzz: " + fizzBuzzCounter + ".")
